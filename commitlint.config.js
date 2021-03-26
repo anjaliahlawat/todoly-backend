ca094ac74c6eed6f039fdb953ba369d214f9f9d4
anjali <anjaliahlawat>
2021-03-26 09:02:32 +0530

    feat: husky & commitlint setup

---
+++ commitlint.config.js
@@ -0,0 +1,15 @@
+const Configuration = {
+    extends: ["@commitlint/config-conventional"],
+  
+    rules: {
+      "references-empty": [2, "never"],
+      "type-enum": [
+        2,
+        "always",
+        ["feat", "fix", "docs", "style", "refactor", "test", "revert"],
+      ],
+    },
+  };
+  
+  module.exports = Configuration;
+  
