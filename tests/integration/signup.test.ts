c523455541939e577bd44266c77bff0db97a8bea
anjaliahlawat <anjaliahlawat16@gmail.com>
2021-05-22 19:58:56 +0530

    test: integration test for register new user api [#38]

---
+++ tests/integration/signup.test.ts
@@ -0,0 +1,40 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import * as request from "supertest";
+
+import server from "../../src/index";
+import { User } from "../../src/modals/users";
+
+describe("/api/register", () => {
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  beforeEach(() => {});
+  afterEach(async () => {
+    server.close();
+    await User.remove({});
+  });
+
+  it("should register new user on valid credentials", async () => {
+    const formData = {
+      username: "Shuchi Aggarwal",
+      phoneNumber: "9320112345",
+      email: "shuchi@gmail.com",
+      password: "12345678",
+    };
+
+    const res = await request(server).post("/api/register").send(formData);
+    expect(res.status).toBe(200);
+    expect(res.body).toHaveProperty("user.username", formData.username);
+  });
+
+  it.only("should return error if email id is already registered", async () => {
+    const formData = {
+      username: "Ankur Satya",
+      phoneNumber: "9891972726",
+      email: "anjali@gmail.com",
+      password: "87654321",
+    };
+
+    const res = await request(server).post("/api/register").send(formData);
+    expect(res.status).toBe(400);
+    expect(res.text).toBe("User already registered");
+  });
+});
