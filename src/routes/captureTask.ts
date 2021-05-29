de2ebe3f11ebc25602ed15e6c36e04c171fd32b3
anjaliahlawat <anjaliahlawat16@gmail.com>
2021-05-29 18:51:55 +0530

    refactor: modals renamed to  models [#50]

--- src/routes/captureTask.ts
+++ src/routes/captureTask.ts
@@ -13,55 +13,47 @@ const taskObj = new TaskClass();
 
 router.post(
   "/add",
-  asyncMiddleware(
-    async (req: Request, res: Response): Promise<void> => {
-      const { email, tasks } = req.body;
-      const user = await userObj.getUserId(email);
-      const addedTasks = await taskObj.createTask(tasks, user);
-      const savedTasks = await capturedObj.add(addedTasks);
-      const response = {
-        result: "success",
-        data: savedTasks,
-      };
-      res.send(response);
-    }
-  )
+  asyncMiddleware(async (req: Request, res: Response): Promise<void> => {
+    const { email, tasks } = req.body;
+    const user = await userObj.getUserId(email);
+    const addedTasks = await taskObj.createTask(tasks, user);
+    const savedTasks = await capturedObj.add(addedTasks);
+    const response = {
+      result: "success",
+      data: savedTasks,
+    };
+    res.send(response);
+  })
 );
 
 router.post(
   "/list",
-  asyncMiddleware(
-    async (req: Request, res: Response): Promise<void> => {
-      const { email } = req.body;
-      const user = await userObj.getUserId(email);
-      const createdTasks = await taskObj.getAllTasks(user);
-      const capturedTasks = await capturedObj.getAllTasks(createdTasks);
-      res.send(capturedTasks);
-    }
-  )
+  asyncMiddleware(async (req: Request, res: Response): Promise<void> => {
+    const { email } = req.body;
+    const user = await userObj.getUserId(email);
+    const createdTasks = await taskObj.getAllTasks(user);
+    const capturedTasks = await capturedObj.getAllTasks(createdTasks);
+    res.send(capturedTasks);
+  })
 );
 
 router.post(
   "/edit",
-  asyncMiddleware(
-    async (req: Request, res: Response): Promise<void> => {
-      const { task } = req.body;
-      const updatedTask = await taskObj.updateTask(task);
-      res.send(updatedTask);
-    }
-  )
+  asyncMiddleware(async (req: Request, res: Response): Promise<void> => {
+    const { task } = req.body;
+    const updatedTask = await taskObj.updateTask(task);
+    res.send(updatedTask);
+  })
 );
 
 router.post(
   "/delete",
-  asyncMiddleware(
-    async (req: Request, res: Response): Promise<void> => {
-      const { task } = req.body;
-      await taskObj.delete(task);
-      await capturedObj.deleteTask(task);
-      res.send({ result: "success" });
-    }
-  )
+  asyncMiddleware(async (req: Request, res: Response): Promise<void> => {
+    const { tasks } = req.body;
+    await taskObj.deleteAll(tasks);
+    await capturedObj.deleteAll(tasks);
+    res.send({ result: "success" });
+  })
 );
 
 module.exports = router;
